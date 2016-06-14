b4b0bd1156af0afa851a2bfd2f9bfc3bba88d45e
Tiago Miranda <mv2axx@gmail.com>
2016-06-14 22:02:30 +0100

    New release 1.0.7 and changing license to GPL3 since this fork shares no similarities with the original repo source apart from eclipse generated standard update site structure.

--- plugin/src/net/leaotech/propertieseditor/Activator.java
+++ plugin/src/net/leaotech/propertieseditor/Activator.java
@@ -4,57 +4,30 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle.
- * 
- * @author Piotr Wolny
- */
 public class Activator extends AbstractUIPlugin {
-    /**
-     * The plug-in ID.
-     */
-    public static String PLUGIN_ID;
 
-    /**
-     * The shared instance.
-     */
-    private static Activator plugin;
+	public static String PLUGIN_ID;
 
-    /**
-     * Default constructor.
-     */
-    public Activator() {
-    }
+	private static Activator plugin;
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-        Activator.PLUGIN_ID = context.getBundle().getSymbolicName();
-    }
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		Activator.PLUGIN_ID = context.getBundle().getSymbolicName();
+	}
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
 
-    /**
-     * Returns the shared instance.
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-        return plugin;
-    }
+	public static Activator getDefault() {
+		return plugin;
+	}
 
-    /**
-     * Returns an image descriptor for the image file at the given plug-in relative path.
-     * 
-     * @param path the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(String path) {
-        return imageDescriptorFromPlugin(PLUGIN_ID, path);
-    }
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 }
